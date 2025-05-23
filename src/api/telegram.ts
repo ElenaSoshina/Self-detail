e18fddee6fce28e9f45f3a2bbc0733e0d7e591df
ElenaSoshina <soshinaalena6415@gmail.com>
2025-05-23 10:56:28 +0300

    add admin id in array

--- src/api/telegram.ts
+++ src/api/telegram.ts
@@ -1,7 +1,7 @@
 import api from './apiService';
 
 // Массив ID чатов администраторов
-export const ADMIN_CHAT_IDS = ['522814078']; // IDs чатов администраторов
+export const ADMIN_CHAT_IDS = ['522814078', '67030677']; // IDs чатов администраторов
 export const ADMIN_CHAT_ID = ADMIN_CHAT_IDS[0]; // Для обратной совместимости
 
 /**
